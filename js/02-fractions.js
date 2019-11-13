50918edfffd523d17796a141109fb266e50c494e
kuomolew <kuomolew@gmail.com>
2019-11-13 11:24:11 +0200

    changed methods to functions

--- js/02-fractions.js
+++ js/02-fractions.js
@@ -16,111 +16,125 @@ class Fraction {
         this.numerator = numerator;
         this.denominator = denominator;
     }
-
-    addition(fraction) {
-        let num, denom, numFirst, denomFirst, numSecond, denomSecond;
-        numFirst = this.numerator;
-        denomFirst = this.denominator;
-        numSecond = fraction.numerator;
-        denomSecond = fraction.denominator;
-        if (denomFirst !== denomSecond) {
-            denom = denomFirst * denomSecond;
-            numFirst = numFirst * denomSecond;
-            numSecond = numSecond * denomFirst;
-        } else {
-            denom = denomFirst;
-        }
-        num = numFirst + numSecond;
-        this.numerator = num;
-        this.denominator = denom;
-        this.reduction();
-    }
-
-    subtraction(fraction){
-        let num, denom, numFirst, denomFirst, numSecond, denomSecond;
-        numFirst = this.numerator;
-        denomFirst = this.denominator;
-        numSecond = fraction.numerator;
-        denomSecond = fraction.denominator;
-        if (denomFirst !== denomSecond) {
-            denom = denomFirst * denomSecond;
-            numFirst = numFirst * denomSecond;
-            numSecond = numSecond * denomFirst;
-        } else {
-            denom = denomFirst;
-        }
-        num = numFirst - numSecond;
-        this.numerator = num;
-        this.denominator = denom;
-        this.reduction();
-    }
-
-    multiplication(fraction) {
-        let num, denom, numFirst, denomFirst, numSecond, denomSecond;
-        numFirst = this.numerator;
-        denomFirst = this.denominator;
-        numSecond = fraction.numerator;
-        denomSecond = fraction.denominator;
-        num = numFirst * numSecond;
-        denom = denomFirst * denomSecond;
-        this.numerator = num;
-        this.denominator = denom;
-        this.reduction();
-    }
-
-    division(fraction) {
-        let num, denom, numFirst, denomFirst, numSecond, denomSecond;
-        numFirst = this.numerator;
-        denomFirst = this.denominator;
-        numSecond = fraction.denominator;
-        denomSecond = fraction.numerator;
-        num = numFirst * numSecond;
-        denom = denomFirst * denomSecond;
-        this.numerator = num;
-        this.denominator = denom;
-        this.reduction();
-    }
-
-    reduction() {
-        let num, denom, smaller, bigger, divider;
-        num = Math.abs(this.numerator);
-        denom = Math.abs(this.denominator);
-        if (num > denom) {
-            bigger = num;
-            smaller = denom;
-        } else {
-            bigger = denom;
-            smaller = num;
-        }
-
-        for (let i = smaller; i > 0; i--) {
-            if (smaller % i == 0 && bigger % i == 0) {
-                divider = i;
-                break;
-            }
-        }
-        if (divider > 0) {
-            this.numerator = this.numerator / divider;
-            this.denominator = this.denominator / divider;
-        }
-        
-    }
-
-
-    
-
 };
 
-// let _23 = new Fraction(2,3);
-// let _45 = new Fraction(4,5);
-// let _12 = new Fraction(1,2);
-// let _46 = new Fraction(4,6);
-// _23.addition(_45);
-// _12.division(_12);
-// _23.subtraction(_45);
-// _46.reduction();
+let addition = function(a, b) {
+    let num, denom, numFirst, denomFirst, numSecond, denomSecond;
+    numFirst = a.numerator;
+    denomFirst = a.denominator;
+    numSecond = b.numerator;
+    denomSecond = b.denominator;
+    if (denomFirst !== denomSecond) {
+        denom = denomFirst * denomSecond;
+        numFirst = numFirst * denomSecond;
+        numSecond = numSecond * denomFirst;
+    } else {
+        denom = denomFirst;
+    }
+    num = numFirst + numSecond;
+    let sum = new Fraction(num, denom);
+    reduction(sum);
+    return  sum;
+}
 
-let _14 = new Fraction(1,4);
-let _34 = new Fraction(3,4);
-_34.subtraction(_34);
-console.log(_34);
+let subtraction = function(a,b) {
+    let num, denom, numFirst, denomFirst, numSecond, denomSecond;
+    numFirst = a.numerator;
+    denomFirst = a.denominator;
+    numSecond = b.numerator;
+    denomSecond = b.denominator;
+    if (denomFirst !== denomSecond) {
+        denom = denomFirst * denomSecond;
+        numFirst = numFirst * denomSecond;
+        numSecond = numSecond * denomFirst;
+    } else {
+        denom = denomFirst;
+    }
+    num = numFirst - numSecond;
+    let sum = new Fraction(num, denom);
+    reduction(sum);
+    return  sum;
+}
+
+let multiplication = function(a,b) {
+    let num, denom, numFirst, denomFirst, numSecond, denomSecond;
+    numFirst = a.numerator;
+    denomFirst = a.denominator;
+    numSecond = b.numerator;
+    denomSecond = b.denominator;
+    num = numFirst * numSecond;
+    denom = denomFirst * denomSecond;
+    let sum = new Fraction(num, denom);
+    reduction(sum);
+    return  sum;
+}
+
+let division = function(a,b) {
+    let num, denom, numFirst, denomFirst, numSecond, denomSecond;
+    numFirst = a.numerator;
+    denomFirst = a.denominator;
+    numSecond = b.denominator;
+    denomSecond = b.numerator;
+    num = numFirst * numSecond;
+    denom = denomFirst * denomSecond;
+    let sum = new Fraction(num, denom);
+    reduction(sum);
+    return  sum;
+}
+
+
+
+let reduction = function(fraction) {
+    let num, denom, smaller, bigger, divider;
+    num = Math.abs(fraction.numerator);
+    denom = Math.abs(fraction.denominator);
+    if (num > denom) {
+        bigger = num;
+        smaller = denom;
+    } else {
+        bigger = denom;
+        smaller = num;
+    }
+
+    for (let i = smaller; i > 0; i--) {
+        if (smaller % i == 0 && bigger % i == 0) {
+            divider = i;
+            break;
+        }
+    }
+    if (divider > 0) {
+        fraction.numerator = fraction.numerator / divider;
+        fraction.denominator = fraction.denominator / divider;
+    }
+}
+
+let _23 = new Fraction(2,3);
+let _45 = new Fraction(4,5);
+let _12 = new Fraction(1,2);
+let _46 = new Fraction(4,6);
+console.log(addition(_45, _23));
+console.log(division(_12, _12));
+console.log(subtraction(_45, _23));
+
+
+// let _14 = new Fraction(1,4);
+// let _34 = new Fraction(3,4);
+//_34.subtraction(_34);
+//console.log(_34);
+// let check = addition(_34, _14);
+// console.log('check1', check);
+// reduction(check);
+// console.log(check);
+// let check2 = addition(_14, _34);
+// console.log('check2', check2);
+
+// check2 = addition(check2, _14);
+// console.log('check2', check2);
+// console.log('check1', check);
+
+// check2 = subtraction(check2, _14);
+
+// check2 = 1;
+// console.log('check2', check2);
+
+
